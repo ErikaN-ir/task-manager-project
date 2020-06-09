57fca6e688d760a6e8199cb83929e46bfb768fe0
ErikaN-ir <erika.nethaway@gmail.com>
2020-06-09 19:23:43 +0000

    Fixed typo bug

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def hello():
     return 'Hello World ...again'
 
 if __name__ == '__main__':
-    app.run(host-os.environ.get('IP'),
+    app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
